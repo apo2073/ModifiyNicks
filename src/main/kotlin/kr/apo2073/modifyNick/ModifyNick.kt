32b2204e77a2edc3bce60ed92c71a826b93bb36a
APO2073 <santabes93@gmail.com>
2024-12-13 10:26:00 +0900

    init

---
+++ src/main/kotlin/kr/apo2073/modifyNick/ModifyNick.kt
@@ -0,0 +1,14 @@
+package kr.apo2073.modifyNick
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ModifyNick : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
